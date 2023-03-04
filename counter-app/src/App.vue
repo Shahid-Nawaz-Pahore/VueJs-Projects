ff143df3a649b61637ea444b8e3c5009ecefdbfc
Shahid Nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-04 14:45:37 +0500

    interface of counter app is completed

--- counter-app/src/App.vue
+++ counter-app/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main><div>
+ <h1>Current counting ...</h1>
+  <h1>0</h1>
+  <button>+</button>
+  <button>-</button>
+  </div></main>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+main{
+  background-color: aqua;
+}
+div{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
+
