66a2fc6170af9bae557f211f6ec8b8940a3c9539
Shahid Nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-04 15:24:26 +0500

    counting successfully

--- counter-app/src/App.vue
+++ counter-app/src/App.vue
@@ -1,17 +1,41 @@
+<script setup>
+import { ref } from 'vue';
+
+const count=ref(0);
+</script>
+
 <template>
   <main><div>
- <h1>Current counting ...</h1>
-  <h1>0</h1>
-  <button>+</button>
-  <button>-</button>
+  
+  <button @click="count++">+</button>
+  <h1>{{count}}</h1>
+  <button  @click="count--">-</button>
   </div></main>
 </template>
-<style>
+<style scoped>
 main{
   background-color: aqua;
+  height: 100vh;
 }
 div{
+  
   text-align: center;
+  justify-content: center;
 }
+button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 8px;
+  box-shadow: 20px 20px 50px #888888;
+  cursor: pointer;
+  margin: 20px;
+  padding: 20px;
+}
+
 </style>
 
